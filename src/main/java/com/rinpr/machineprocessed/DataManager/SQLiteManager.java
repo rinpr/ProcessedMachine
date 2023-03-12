b86437eae9430bae447648a10f2e4ee9db3c1906
rinpr <duggerseven@gmail.com>
2023-03-12 11:31:21 +0700

    remove unused import

--- src/main/java/com/rinpr/machineprocessed/DataManager/SQLiteManager.java
+++ src/main/java/com/rinpr/machineprocessed/DataManager/SQLiteManager.java
@@ -1,6 +1,5 @@
 package com.rinpr.machineprocessed.DataManager;
 
-import com.rinpr.machineprocessed.MachineProcessed;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
